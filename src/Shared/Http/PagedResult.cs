cd19c22b75e74c558a6a7fafa116cd0d6ed72346
srodriguez4704 <rokket250@gmail.com>
2026-03-18 19:32:34 -0400

    Add HttpUtils

---
+++ src/Shared/Http/PagedResult.cs
@@ -0,0 +1,12 @@
+namespace Shared.Http;
+
+public class PagedResult<T>
+{
+    public int TotalCount { get; }
+    public List<T> Values { get; }
+    public PagedResult(int totalCount, List<T> values)
+    {
+        TotalCount = totalCount;
+        Values = values;
+    }
+}
